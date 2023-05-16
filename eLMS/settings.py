bd509eddca9f6690e59635985636b7a2d25cffa0
Lalith514 <lalithkumaruppada@gmail.com>
2023-05-16 22:24:42 +0530

    helooo

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -83,22 +83,22 @@ WSGI_APPLICATION = 'eLMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'elms',
-#         'HOST': 'localhost',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'HOST': 'containers-us-west-178.railway.app',
+        'USER': 'root',
+        'PASSWORD': 'moW5yBQIIfh9YCojc0sG',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
